a2d9400baba621435f824921d9e05b9ffc6a7ece
alejpaa <alejandro.padillaa@unmsm.edu.pe>
2025-08-31 17:36:02 -0500

    add all endpoints

--- apps/backend/src/url/url.controller.ts
+++ apps/backend/src/url/url.controller.ts
@@ -1,11 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { UrlService } from './url.service';
 
 @Controller()
 export class UrlController {
-    
-    @Get('/health')
-    getHealth(){
-        return "ok"
-    }
-    
+  constructor(private urlService: UrlService) {}
+
+  @Get('/health')
+  getHealth() {
+    return this.urlService.health();
+  }
+
+  @Post('/shorten')
+  shorten() {
+    return this.urlService.shorten();
+  }
+
+  @Get('/slug')
+  getSlug() {
+    return this.urlService.getSlug();
+  }
 }
--- apps/backend/src/url/url.service.ts
+++ apps/backend/src/url/url.service.ts
@@ -2,6 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UrlService {
+  health() {
+    return 'Ok';
+  }
 
-    
+  shorten() {
+    return 'in process XD';
+  }
+
+  getSlug() {
+    return 'Esta es tu url xd por ahora';
+  }
 }
