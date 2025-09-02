bbc083c34933a0db9e7f1a4c7544751c2f969de4
alejpaa <alejandro.padillaa@unmsm.edu.pe>
2025-09-01 23:56:21 -0500

    refactor: remove unused ValidationPipe and enhance CORS configuration

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,11 +1,16 @@
-import { ValidationPipe } from '@nestjs/common';
+// import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: ['http://localhost:4321', 'http://127.0.0.1:4321'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
 
   app.setGlobalPrefix('/api/v1');
 
